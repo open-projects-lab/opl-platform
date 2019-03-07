7fc2d37392e1222a95288372bd04ed95d9da4f66
tayyabRazzaq <tayyab.razzaq401@gmail.com>
2019-03-07 10:42:00 +0500

    feat: Higher order app created

--- leaderboard/settings.py
+++ leaderboard/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+LEADERBOARD_APPS = [
+    'apps'
+]
+
+INSTALLED_APPS = DJANGO_APPS + LEADERBOARD_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
