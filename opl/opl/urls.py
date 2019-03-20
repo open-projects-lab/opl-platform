8c3051859b2b4171ca9504bffef288e6425bbedc
Irzelindo <irzelindo.salvador@gmail.com>
2019-03-20 18:13:14 +0200

    Add urls file to core app

--- opl/opl/urls.py
+++ opl/opl/urls.py
@@ -20,7 +20,11 @@ from django.contrib import admin
 admin.site.index_title = 'OPL Database'
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^nested_admin/', include('nested_admin.urls')),
-    url(r'^grappelli/', include('grappelli.urls')),
+    url('admin/', admin.site.urls),
+    url('nested_admin/', include('nested_admin.urls')),
+    url('grappelli/', include('grappelli.urls')),
+    path('register/', users_views.register, name='register'),
+    path('profile/', users_views.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'),
 ]
